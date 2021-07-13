d071eaaa56f007ad30b448cda6023cbba1249d2f
CleydsonSl <cley.souza007@gmail.com>
2021-07-13 19:03:46 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  state = {
+    texto: 'Turma React'
+  }
+  handleClick() {
+    alert('Alerta Oiii')
+  }
+  handleTextChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World! {this.state.texto}</h1>
+        <input type="text" onChange={this.handleTextChange}></input>
+        <button onClick={this.handleClick.bind(this)}>Click</button>
+      </div>
+
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name={"Cleydson Souza"} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
